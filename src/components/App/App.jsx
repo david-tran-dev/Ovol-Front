28a2e3ca9d2fd8773b3b0cc1b472ca286d8f66dc
David TRAN <david.tran.devweb@gmail.com>
2022-02-25 16:31:08 +0100

    fix: mergePB

--- src/components/App/App.jsx
+++ src/components/App/App.jsx
@@ -5,13 +5,8 @@ import Header from '../Header/Header';
 function App() {
   return (
     <div className="App">
-<<<<<<< HEAD:src/components/App/App.jsx
       <Header className="fa-solid fa-circle-user" />
-=======
-      <h1>Coucou</h1>
-      <i className="fa-solid fa-circle-user" />
       <Login />
->>>>>>> feature/loginPage:src/components/App/App.js
     </div>
   );
 }
