07a23ea58341f71650f0682664a4dd1a07fecf56
David TRAN <david.tran.devweb@gmail.com>
2022-02-28 12:00:50 +0100

    feat(router): add tracksList and track routes

--- src/components/App/App.jsx
+++ src/components/App/App.jsx
@@ -2,6 +2,7 @@ import { Login } from '@mui/icons-material';
 import { Routes, Route, useLocation } from 'react-router-dom';
 import Header from '../Header/Header';
 import Map from '../Map/Map';
+import TracksList from '../TracksList/TracksList';
 import './App.css';
 
 function App() {
@@ -13,6 +14,8 @@ function App() {
       <Routes location={location}>
         <Route path="/" element={<Map />} />
         <Route path="/login" element={<Login />} />
+        <Route path="/tracksList" element={<TracksList />} />
+        <Route path="/track/:id" element={<Track />} />
       </Routes>
     </div>
   );
