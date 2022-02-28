4e3004b01079cb005a0fe17484f68460cb9beaf2
David TRAN <david.tran.devweb@gmail.com>
2022-02-28 11:41:30 +0100

    feat(router): add react router and login route

--- src/components/App/App.jsx
+++ src/components/App/App.jsx
@@ -1,11 +1,19 @@
+import { Login } from '@mui/icons-material';
+import { Routes, Route, useLocation } from 'react-router-dom';
+import Header from '../Header/Header';
 import Map from '../Map/Map';
 import './App.css';
 
 function App() {
+  const location = useLocation();
+
   return (
     <div className="App">
-
-      <Map />
+      <Header />
+      <Routes location={location}>
+        <Route path="/" element={<Map />} />
+        <Route path="/login" element={<Login />} />
+      </Routes>
     </div>
   );
 }
