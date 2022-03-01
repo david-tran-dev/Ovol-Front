b83418effcfa70bfa147b954a58c79051f04ec23
David TRAN <david.tran.devweb@gmail.com>
2022-03-01 19:46:49 +0100

    feat: add buttons to track

--- src/components/Track/Track.jsx
+++ src/components/Track/Track.jsx
@@ -1,11 +1,13 @@
+/* eslint-disable react/no-danger */
 /* eslint-disable react/no-array-index-key */
 import React from 'react';
 import PropTypes from 'prop-types';
 import './track.scss';
 import { Navigate, useParams } from 'react-router-dom';
 import {
-  CardMedia, Container, Icon, Typography, Box,
+  CardMedia, Container, Icon, Typography, Box, Button,
 } from '@mui/material';
+import DOMPurify from 'dompurify';
 
 function Track({ className, tracksList, ...rest }) {
   const { id } = useParams();
@@ -15,7 +17,6 @@ function Track({ className, tracksList, ...rest }) {
   if (!trackFinded) {
     return <Navigate to="/error" replace />;
   }
-
   const steps = trackFinded.key_stage.split('\n');
   console.log('steps:', steps);
   console.log('render');
@@ -26,41 +27,71 @@ function Track({ className, tracksList, ...rest }) {
     >
       <Container sx={{ my: 1 }}>
         <CardMedia
+          className="track-img"
           component="img"
           height="140"
           image={`${trackFinded.img_card}`}
           alt="hiking image"
+          title={trackFinded.name}
         />
-        <Typography gutterBottom variant="h5" textAlign="left"> {trackFinded.name}</Typography>
+        <Typography gutterBottom variant="h5" textAlign="left" fontWeight="bold"> {trackFinded.name}</Typography>
         <Box sx={{ display: 'flex', textAlign: 'left', my: 1 }}>
           <Box sx={{ pr: 2, width: '50%' }}>
-            <Typography variant="h6" sx={{ mb: 1 }}>
+            <Typography variant="h6" sx={{ mb: 1, fontWeight: 'bold' }}>
               Infos techniques:
             </Typography>
-            <Typography>Dénivelé positif: {trackFinded.positive_elevation}m</Typography>
-            <Typography>Dénivelé négatif: {trackFinded.negative_elevation}m</Typography>
-            <Typography>Point haut: {trackFinded.hight_point}m</Typography>
-            <Typography>Point bas: {trackFinded.low_point}m</Typography>
-            <Typography>Carte IGN: {trackFinded.ign_card_reference}</Typography>
-            <Typography>Terrain: {trackFinded.land_type}</Typography>
+            <Typography className="track-info__key">Dénivelé positif:
+              <Typography className="track-info__value" component="span"> {trackFinded.positive_elevation}m</Typography>
+            </Typography>
+            <Typography className="track-info__key">Dénivelé négatif:
+              <Typography className="track-info__value" component="span"> {trackFinded.negative_elevation}m</Typography>
+            </Typography>
+            <Typography className="track-info__key">Point haut:
+              <Typography className="track-info__value" component="span"> {trackFinded.hight_point}m</Typography>
+            </Typography>
+            <Typography className="track-info__key">Point bas:
+              <Typography className="track-info__value" component="span"> {trackFinded.low_point}m</Typography>
+            </Typography>
+            <Typography className="track-info__key">Carte IGN:
+              <Typography className="track-info__value" component="span"> {trackFinded.ign_card_reference}</Typography>
+            </Typography>
+            <Typography className="track-info__key">Terrain:
+              <Typography className="track-info__value" component="span"> {trackFinded.land_type}</Typography>
+            </Typography>
 
           </Box>
-          <Box sx={{ width: '50%' }}>
-            <Box sx={{ display: 'flex', justifyContent: 'flex-end', mb: 2 }}>
+          <Box sx={{ width: '50%', mb: 2 }}>
+            <Box sx={{
+              display: 'flex', justifyContent: 'flex-end', alignItems: 'baseline', mb: 2,
+            }}
+            >
               <Icon className="fa-solid fa-flag" sx={{ width: 24, height: 24, mx: 1 }} />
               <Icon className="fa-solid fa-map-location-dot" sx={{ width: 24, height: 24, mx: 1 }} />
             </Box>
-            <Typography>Massif: {trackFinded.mountain}</Typography>
-            <Typography>Point de départ: {trackFinded.starting_point}</Typography>
-            <Typography>Distance: {trackFinded.overall_length}km</Typography>
-            <Typography>Difficulté: {trackFinded.difficulty}</Typography>
+            <Typography className="track-info__key">Massif:
+              <Typography className="track-info__value" component="span"> {trackFinded.mountain}</Typography>
+            </Typography>
+            <Typography className="track-info__key">Point de départ:
+              <Typography className="track-info__value" component="span"> {trackFinded.starting_point}</Typography>
+            </Typography>
+            <Typography className="track-info__key">Distance:
+              <Typography className="track-info__value" component="span"> {trackFinded.overall_length}km</Typography>
+            </Typography>
+            <Typography className="track-info__key">Difficulté:
+              <Typography className="track-info__value" component="span"> {trackFinded.difficulty}</Typography>
+
+            </Typography>
           </Box>
         </Box>
-        <Typography variant="h5" gutterBottom gutterTop textAlign="left">Résumé</Typography>
-        <Typography gutterBottom textAlign="left"> {trackFinded.resume} </Typography>
-        <Typography variant="h5" gutterBottom gutterTop textAlign="left">Etapes de la randonnée</Typography>
+        <Typography variant="h5" gutterBottom textAlign="left" fontWeight="bold">Résumé</Typography>
+        <Typography gutterBottom textAlign="left" sx={{ mb: 2 }}> {trackFinded.resume} </Typography>
+        <Typography variant="h5" gutterBottom textAlign="left" fontWeight="bold">Etapes de la randonnée</Typography>
         {steps.map((step, index) => <Typography key={step + index} textAlign="left">{step}</Typography>)}
-        <div className="track-hiking-map" dangerouslySetInnerHTML={{ __html: '<iframe src="https://www.google.com/maps/d/embed?mid=1xD4Ypl3aj1YlMS2tHSHkquLROQbSuKOM&ehbc=2E312F" width="640" height="480"></iframe>' }} />
+        <div className="track-hiking-map" dangerouslySetInnerHTML={{ __html: DOMPurify.sanitize(trackFinded.hiking_plan, { ALLOWED_TAGS: ['iframe'] }) }} />
+        <Box sx={{ display: 'flex', mt: 2, justifyContent: 'space-around' }}>
+          <Button variant="contained">Décollage</Button>
+          <Button variant="contained">Attérissage</Button>
+        </Box>
       </Container>
     </div>
   );
@@ -68,6 +99,22 @@ function Track({ className, tracksList, ...rest }) {
 
 Track.propTypes = {
   className: PropTypes.string,
+  tracksList: PropTypes.arrayOf(
+    PropTypes.shape({
+      id: PropTypes.number.isRequired,
+      difficulty: PropTypes.string.isRequired,
+      mountain: PropTypes.string.isRequired,
+      positive_elevation: PropTypes.number.isRequired,
+      negative_elevation: PropTypes.number.isRequired,
+      overall_length: PropTypes.number.isRequired,
+      img_card: PropTypes.string.isRequired,
+      starting_point: PropTypes.string.isRequired,
+      hiking_plan: PropTypes.string,
+      resume: PropTypes.string.isRequired,
+      hight_point: PropTypes.number.isRequired,
+      low_point: PropTypes.number.isRequired,
+    }),
+  ).isRequired,
 
 };
 Track.defaultProps = {
