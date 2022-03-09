f3b4908324794d6823ec033dea1fbaa81cea1768
Adrien35240 <adriendev35240@gmail.com>
2022-03-09 16:22:26 +0100

    feat(compass): add wind instant from balise

--- src/components/Compass/Compass.jsx
+++ src/components/Compass/Compass.jsx
@@ -1,8 +1,10 @@
+/* eslint-disable no-mixed-operators */
 /* eslint-disable react/prop-types */
 import { useState, useEffect } from 'react';
+import axios from 'axios';
 import Graph from './Graph';
 
-function Compass({ favorableWind, unfavorableWind }) {
+function Compass({ favorableWind, unfavorableWind, balise }) {
   const [data, setData] = useState([]);
   const [windDirection, setWindDirection] = useState('');
   // valeur par default
@@ -120,42 +122,71 @@ function Compass({ favorableWind, unfavorableWind }) {
           }
         });
       }
-      // wind direction
+      //  wind direction
       // si la valeur du select est egale a data[index].C ,on le set au max(ici 150) et l'index +/- 1 à 40
-      // if (item.subject === windDirection) {
+      if (item.subject === windDirection && windDirection !== undefined) {
       // exception pour le NO dont l'index+1 serait undefined car superieur a data.length
-      // if (item.subject === 'NO') {
-      //   const newArray = [...data];
-      //   newArray[index].C = 150;
-      //   newArray[0].C = 40;
-      //   newArray[index - 1].C = 40;
-      //   setData(newArray);
-      // }
-      // exception pour le N dont l'index-1 serait undefined car inferieur a 0
-      // else if (item.subject === 'N') {
-      //   const newArray = [...data];
-      //   newArray[index].C = 150;
-      //   newArray[index + 1].C = 40;
-      //   newArray[7].C = 40;
-      //   setData(newArray);
-      // }
-      // else {
-      //   const newArray = [...data];
-      //   newArray[index].C = 150;
-      //   newArray[index + 1].C = 40;
-      //   newArray[index - 1].C = 40;
-      //   setData(newArray);
-      // }
-      // }
+        if (item.subject === 'NO') {
+          const newArray = [...data];
+          newArray[index].C = 150;
+          newArray[0].C = 40;
+          newArray[index - 1].C = 40;
+          setData(newArray);
+        }
+        // exception pour le N dont l'index-1 serait undefined car inferieur a 0
+        else if (item.subject === 'N') {
+          const newArray = [...data];
+          newArray[index].C = 150;
+          newArray[index + 1].C = 40;
+          newArray[7].C = 40;
+          setData(newArray);
+        }
+        else {
+          const newArray = [...data];
+          newArray[index].C = 150;
+          newArray[index + 1].C = 40;
+          newArray[index - 1].C = 40;
+          setData(newArray);
+        }
+      }
     });
     // console.log('after change', data);
   }
-  // TODO : get wind direction from api
+  async function getBaliseInfos() {
+    const result = await axios.get(`https://balisemeteo.com/balise_json.php?idBalise=${balise}}`);
+    const windInstant = result.data.directVentInst;
+    if (windInstant === undefined) {
+      setWindDirection('undefined');
+    }
+    if (windInstant > 0 && windInstant < 22.5 || windInstant >= 337.5) {
+      setWindDirection('N');
+    }
+    if (windInstant >= 22.5 && windInstant < 67.5) {
+      setWindDirection('NE');
+    }
+    if (windInstant >= 67.5 && windInstant < 112.5) {
+      setWindDirection('E');
+    }
+    if (windInstant >= 112.5 && windInstant < 157.5) {
+      setWindDirection('SE');
+    }
+    if (windInstant >= 157.5 && windInstant < 202.5) {
+      setWindDirection('S');
+    }
+    if (windInstant >= 202.5 && windInstant < 247.5) {
+      setWindDirection('SO');
+    }
+    if (windInstant >= 247.5 && windInstant < 292.5) {
+      setWindDirection('O');
+    }
+    if (windInstant >= 292.5 && windInstant < 337.5) {
+      setWindDirection('NO');
+    }
+  }
   useEffect(() => {
     setData(dataApi);
-    setWindDirection('NO');
+    getBaliseInfos();
     handleGraphData();
-    // console.log('before change', dataApi);
   }, [windDirection]);
 
   return (
