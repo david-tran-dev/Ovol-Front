3f2d1fc3e3a388cb2d9294ee0f9405bb8436bc3c
David TRAN <david.tran.devweb@gmail.com>
2022-03-15 21:35:59 +0100

    fix: pre fill the state for youtube demo

--- src/components/AdminCreate/AdminCreate.jsx
+++ src/components/AdminCreate/AdminCreate.jsx
@@ -47,56 +47,116 @@ function AdminCreate({
   const navigate = useNavigate();
 
   useEffect(async () => {
-    setLoading(true);
+    // setLoading(true);
+    // if (Object.keys(liftOff).length === 0) {
+    //   const response = await requestLiftOff(2);
+    //   console.log('response setloadn', response);
+    //   if (response.status === 200) {
+    //     setLiftOff(response.data[0]);
+    //   }
+    //   else {
+    //     console.log(response);
+    //     navigate('/error');
+    //   }
+    // }
+    // if (Object.keys(liftOff).length > 0) {
+    //   setLoading(false);
+    // }
     if (Object.keys(liftOff).length === 0) {
-      const response = await requestLiftOff(2);
-      console.log('response setloadn', response);
-      if (response.status === 200) {
-        setLiftOff(response.data[0]);
-      }
-      else {
-        console.log(response);
-        navigate('/error');
-      }
-    }
-    if (Object.keys(liftOff).length > 0) {
-      setLoading(false);
+      const deco = {
+        id: 2,
+        name: 'Déco SEPTMONCEL',
+        typeOfTerrain: 'Herbe',
+        description: "Approche de l'atterrissage de l'Essard (contre-pente) en PT8 pour le delta. L'atterrissage du haut est délicat en parapente en pleine journée. Toujours vérifier que la biroute du décollage parapente est bien orientée pour éviter de décoller dans le rouleau en cas de vent Est ou Nord. Les parapentes décollent à 1050 m. Les deltas décollent à 1020 m.",
+        danger: 'Ligne à haute tension au milieu du cirque.',
+        fflvLink: 'https://intranet.ffvl.fr/sites_pratique/voir/54',
+        latitude: 46.371,
+        longitude: 5.8985,
+        favorableWind: 'O',
+        unfavorableWind: '',
+        balise: 2563,
+        altitude: 1026,
+        photo_liftOff: [],
+      };
+      setValuesLiftOff(deco);
+      setLiftOff(deco);
     }
   }, [liftOff]);
 
   useEffect(async () => {
-    setLoading(true);
+    // setLoading(true);
+    // if (Object.keys(landing).length === 0) {
+    //   const response = await requestLandings(2);
+    //   console.log('response setloadn', response);
+    //   if (response.status === 200) {
+    //     setLanding(response.data[0]);
+    //   }
+    //   else {
+    //     console.log(response);
+    //     navigate('/error');
+    //   }
+    // }
+    // if (Object.keys(landing).length > 0) {
+    //   setLoading(false);
+    // }
     if (Object.keys(landing).length === 0) {
-      const response = await requestLandings(2);
-      console.log('response setloadn', response);
-      if (response.status === 200) {
-        setLanding(response.data[0]);
-      }
-      else {
-        console.log(response);
-        navigate('/error');
-      }
-    }
-    if (Object.keys(landing).length > 0) {
-      setLoading(false);
+      const atero = {
+        id: 2,
+        name: 'Atéro Villard',
+        typeOfTerrain: 'Herbe',
+        description: 'Turbulent par vent fort, surtout de Nord.',
+        danger: '',
+        fflvLink: 'https://intranet.ffvl.fr/sites_pratique/voir/54',
+        latitude: 46.3723,
+        longitude: 5.87678,
+        favorableWind: 'O',
+        unfavorableWind: '',
+        altitude: 576,
+        photo_landing: [],
+      };
+      setValuesLanding(atero);
+      setLanding(atero);
     }
   }, [landing]);
 
   useEffect(async () => {
-    setLoading(true);
+    // setLoading(true);
+    // if (Object.keys(hiking).length === 0) {
+    //   const response = await requestHiking(2);
+    //   console.log('response setloadn', response);
+    //   if (response.status === 200) {
+    //     setHiking(response.data[0]);
+    //   }
+    //   else {
+    //     console.log(response);
+    //     navigate('/error');
+    //   }
+    // }
+    // if (Object.keys(hiking).length > 0) {
+    //   setLoading(false);
+    // }
     if (Object.keys(hiking).length === 0) {
-      const response = await requestHiking(2);
-      console.log('response setloadn', response);
-      if (response.status === 200) {
-        setHiking(response.data[0]);
-      }
-      else {
-        console.log(response);
-        navigate('/error');
-      }
-    }
-    if (Object.keys(hiking).length > 0) {
-      setLoading(false);
+      const rando = {
+        id: 1,
+        name: 'Semnoz Grotte des fours',
+        img_card: 'https://res.cloudinary.com/ovol/image/upload/q_50/v1646905878/img-seeding/Annecy-Semnoz__2013_hyn3lu.jpg',
+        mountain: 'Bauges',
+        resume: 'Montée soutenue en forêt avec en bonus des grottes à visiter. Attention falaise à surmonter au niveau de la grotte des four. Le sentier est balisé sur toute la longueur',
+        key_stage: "D/A : km 0 - alt. 595m - Cimetière de Viuz-la-Chiésaz\n    1 : km 0.24 - alt. 594m - Croisement CD5/Route des Pierres\n    2 : km 1.61 - alt. 738m - Les Granges\n    3 : km 3.25 - alt. 1016m - Croisement de Combe Noire\n    4 : km 4.22 - alt. 1326m - Croisement de la Grotte des Fours\n    5 : km 5.47 - alt. 1640m - Parking près du bâtiment du Courant d'Ere\n    6 : km 6.13 - alt. 1694m - Crêt de Châtillon - Le Semnoz",
+        starting_point: 'https://goo.gl/maps/WQPNLEjkxyw5KAbv7',
+        hiking_plan: '<iframe src="https://www.google.com/maps/d/u/2/embed?mid=1JZCA93VmwEXRcKn9GnlKawxOtpHfI20W&ehbc=2E312F" width="640" height="480"></iframe>',
+        positive_elevation: 1028,
+        negative_elevation: 2,
+        overall_length: 6.9,
+        land_type: 'Forestier',
+        ign_card_reference: 'Ref. 3431OT 3431OTR',
+        hight_point: 1694,
+        low_point: 587,
+        difficulty: 'Difficile',
+        photo_hiking: [],
+      };
+      setValuesHiking(rando);
+      setHiking(rando);
     }
   }, [hiking]);
 
@@ -156,11 +216,13 @@ function AdminCreate({
     setIsRequired(true);
   };
   const handleChangeLanding = (item, inputValue) => {
+    console.log('item:', item);
+    console.log('inputValue:', inputValue);
     const valueClone = { ...valuesLanding };
     valueClone[item] = inputValue;
     setValuesLanding(valueClone);
     setIsRequired(true);
-    // console.log('le state', value);
+    console.log('le state', valuesLanding);
   };
   const handleChangeHiking = (item, inputValue) => {
     const valueClone = { ...valuesHiking };
@@ -170,7 +232,6 @@ function AdminCreate({
     setIsRequired(true);
   };
   const handleImg = (name, imgName) => {
-    console.log(' C EST QUOI LE NAME ', name);
     if (name === 'landing') {
       const myArrayImgNameClone = [...valuesImgLanding];
       myArrayImgNameClone.push(imgName);
@@ -228,7 +289,7 @@ function AdminCreate({
                     ATTERISSAGE ITEMS
                   </Box>
                   {
-                    Object.keys(landing).map(((item, index) => {
+                    Object.keys(valuesLanding).map(((item, index) => {
                       if (item !== 'id' && item !== 'photo_landing') {
                         return (
                           <TextField
@@ -236,12 +297,26 @@ function AdminCreate({
                             sx={{ p: '2px 4px', width: '100%' }}
                             required
                             label={item}
+                            value={valuesLanding[item]}
                             placeholder={item}
                             onChange={(event) => handleChangeLanding(item, event.target.value)}
                           />
                         );
                       }
+                      // if (item !== 'id' && item !== 'photo_landing') {
+                      //   return (
+                      //     <TextField
+                      //       key={index + item}
+                      //       sx={{ p: '2px 4px', width: '100%' }}
+                      //       required
+                      //       label={item}
+                      //       placeholder={item}
+                      //       onChange={(event) => handleChangeLanding(item, event.target.value)}
+                      //     />
+                      //   );
+                      // }
                       if (item === 'photo_landing') {
+                        console.log('dans le if photo');
                         return (
                           <>
                             <ImageList
@@ -281,7 +356,7 @@ function AdminCreate({
                     DECOLLAGE ITEMS
                   </Box>
                   {
-                    Object.keys(liftOff).map(((item) => {
+                    Object.keys(valuesLiftOff).map(((item) => {
                       if (item !== 'id' && item !== 'idLandings' && item !== 'photo_liftOff') {
                         return (
                           <TextField
@@ -290,6 +365,7 @@ function AdminCreate({
                             label={item}
                             placeholder={item}
                             name={item}
+                            value={valuesLiftOff[item]}
                             onChange={(event) => handleChangeLiftOff(item, event.target.value)}
                           />
                         );
@@ -341,6 +417,7 @@ function AdminCreate({
                             sx={{ p: '2px 4px', width: '100%' }}
                             required
                             label={item}
+                            value={valuesHiking[item]}
                             placeholder={item}
                             onChange={(event) => handleChangeHiking(item, event.target.value)}
                             multiline
