5e7e8a7edc4e1825bf537f0e5df81a40248feb5c
Fred'Dev <fredchenot@gmail.com>
2022-03-15 09:27:51 +0100

    feat: ux pages of ovol

--- src/components/Contact/Contact.jsx
+++ src/components/Contact/Contact.jsx
@@ -11,9 +11,15 @@ import ImageListItem from '@mui/material/ImageListItem';
 import SendIcon from '@mui/icons-material/Send';
 import FaceIcon from '@mui/icons-material/Face';
 import { ThemeProvider } from '@mui/material';
+import Avatar from '@mui/material/Avatar';
 import customTheme from '../../themes/customTheme';
 import itemData from '../../assets/img/itemDataContact/itemDataContact';
 import './contact.scss';
+import fred from '../../assets/img/avatarData/fred.jpg';
+import olivier from '../../assets/img/avatarData/olivier.jpg';
+import adrien from '../../assets/img/avatarData/adrien.jpg';
+import antoine from '../../assets/img/avatarData/antoine.jpg';
+import david from '../../assets/img/avatarData/david.jpg';
 
 function MiddleDividers() {
   return (
@@ -41,17 +47,60 @@ function MiddleDividers() {
             </Grid>
           </Grid>
           <Typography color="text.secondary" variant="body2">
-            Pour nous Contacter :
+            Pour nous Contacter Linkedin et Email :
           </Typography>
         </Box>
         <Divider variant="middle" />
         <Box sx={{ m: 2 }}>
           <Stack direction="row" spacing={1} sx={{ flexWrap: 'wrap', justifyContent: 'center' }}>
-            <Chip className="header-contact__chip" icon={<FaceIcon />} label="David" />
-            <Chip className="header-contact__chip" icon={<FaceIcon />} label="Antoine" />
-            <Chip className="header-contact__chip" icon={<FaceIcon />} label="Olivier" />
-            <Chip className="header-contact__chip" icon={<FaceIcon />} label="Adrien" />
-            <Chip className="header-contact__chip" icon={<FaceIcon />} label="Fred" />
+            <a href="https://www.linkedin.com/in/david-tran-dev/" target="_blank" rel="noreferrer">
+              <Chip
+                clickable
+                className="header-contact__chip"
+                avatar={<Avatar alt="David" src={david} />}
+                icon={<FaceIcon />}
+                label="David"
+              />
+            </a>
+            <a href="https://www.linkedin.com/in/antoine-chul" target="_blank" rel="noreferrer">
+              <Chip
+                clickable
+                avatar={<Avatar alt="Antoine" src={antoine} />}
+                className="header-contact__chip"
+                icon={<FaceIcon />}
+                label="Antoine"
+              />
+            </a>
+
+            <a href="https://www.linkedin.com/in/olivier-reboul-052281140/" target="_blank" rel="noreferrer">
+              <Chip
+                clickable
+                className="header-contact__chip"
+                icon={<FaceIcon />}
+                avatar={<Avatar alt="Olivier" src={olivier} />}
+                label="Olivier"
+              />
+            </a>
+
+            <a href="https://www.linkedin.com/in/aribault35240" target="_blank" rel="noreferrer">
+              <Chip
+                clickable
+                className="header-contact__chip"
+                icon={<FaceIcon />}
+                avatar={<Avatar alt="Adrien" src={adrien} />}
+                label="Adrien"
+              />
+            </a>
+
+            <a href="http://www.linkedin.com/in/frederick-chenot" target="_blank" rel="noreferrer">
+              <Chip
+                clickable
+                avatar={<Avatar alt="Fred" src={fred} />}
+                className="header-contact__chip"
+                icon={<FaceIcon />}
+                label="Fred"
+              />
+            </a>
           </Stack>
         </Box>
         <ThemeProvider theme={customTheme}>
